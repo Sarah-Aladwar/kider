d51e49ea1406970f40d9bd17e2db17c6f11db816
sara <sarahaladwar@gmail.com>
2023-12-10 21:30:45 +0100

    session 10

---
+++ resources/views/kider.blade.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        
+    @include('includes.spinner')
+
+    @include('includes.navbar')
+
+    @include('includes.carousel')
+
+    @include('includes.facilitates')
+
+    @include('includes.about')
+
+    @include('includes.action')
+
+    @include('includes.classes')
+
+    @include('includes.appointment')
+
+    @include('includes.team')
+
+    @include('includes.testimonial')  
+
+    @include('includes.footer')
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    @include('includes.footerjs')
+
+    
+</body>
+
+</html>
